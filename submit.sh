a51029d9bbb5d6fa81c23023d6d1982cd6c7cf7c
Fantine Huot <fantine.huot@gmail.com>
2021-04-02 14:05:45 +0200

    Add example Slurm script

--- submit.sh
+++ submit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-#SBATCH --job-name=test
-#SBATCH --time=00:10:00
+#SBATCH --job-name=job_%j
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100:4
+#SBATCH --time=04:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2G
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=24G
 #SBATCH --output=job_output_%j.out
 #SBATCH --error=job_output_%j.err
 
-srun hostname
-srun lscpu
-srun sleep 60
+srun singularity exec --nv -B /scratch/fantine:/scratch/fantine ml_framework_latest.sif gpustat
